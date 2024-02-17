a81aca197b8be6c7c3a266dd35b68ba8d8b104d2
jackman007-web <70363485+jackman007-web@users.noreply.github.com>
2024-02-17 18:45:59 +0530

    SpeakGoodBye.js

---
+++ SpeakGoodBye.js
@@ -0,0 +1,10 @@
+(function (window) {
+  var byeSpeaker = {};
+  byeSpeaker.speakWord = "Good Bye";
+  byeSpeaker.speak = function (name) {
+    console.log(this.speakWord + " " + name);
+  }
+
+  window.byeSpeaker = byeSpeaker;
+
+})(window);
